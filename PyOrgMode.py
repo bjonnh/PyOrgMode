29f39e8660134434b616469269ce2e6256811fcc
Jonathan BISSON <bissonjonathan@gmail.com>
2010-12-19 18:39:48 +0100

    v0.01h (now with Plug-ins)

--- PyOrgMode.py
+++ PyOrgMode.py
@@ -29,6 +29,19 @@ representation of the file allows the use of orgfiles easily in your projects.
 import re
 import string
 
+class Plugin:
+    """
+    Generic class for all plugins
+    """
+    def __init__(self):
+        self.treated = True
+    def treat(self,current,line):
+        self.treated = True
+        return self._treat(current,line)
+    def _treat(self,current,line):
+        self.treated = False
+        return current
+
 class OrgElement:
     """
     Generic class for all Elements excepted text and unrecognized ones
@@ -36,6 +49,7 @@ class OrgElement:
     def __init__(self):
         self.content=[]
         self.parent=None
+        self.level=0
     def append(self,element):
         # TODO Check validity
         self.content.append(element)
@@ -45,169 +59,224 @@ class OrgElement:
             element.parent = self
         return element
 
-class Property(OrgElement):
-    """
-    A Property object, used in drawers.
-    """
-    def __init__(self,name="",value=""):
-        OrgElement.__init__(self)
-        self.name = name
-        self.value = value
-    def __str__(self):
-        """
-        Outputs the property in text format (e.g. :name: value)
-        """
-        return ":" + self.name + ": " + self.value
-
-class Schedule(OrgElement):
-    """
-    Schedule is an element taking into account DEADLINE and SCHEDULED elements
-    """
-    DEADLINE = 1
-    SCHEDULED = 2
-    def __init__(self,type="",date=""):
-        OrgElement.__init__(self)
-        self.date = date
-        self.type = 0
-        if type == "DEADLINE":
-            self.type = self.DEADLINE
-        elif type == "SCHEDULED":
-            self.type = self.SCHEDULED
-
-    def __str__(self):
-        """
-        Outputs the Schedule element in text format (e.g SCHEDULED: <2010-10-10 10:10>)
-        """
-        if self.type == self.DEADLINE:
-            output = "DEADLINE:"
-        elif self.type == self.SCHEDULED:
-            output = "SCHEDULED:"
-        return output + " " + self.date + "\n"
-
-class Drawer(OrgElement):
-    """
-    A Drawer object, containing properties and text
-    """
-    # TODO has_property, get_property
-    def __init__(self,name=""):
-        OrgElement.__init__(self)
-        self.name = name
-    def __str__(self):
-        output = ":" + self.name + ":\n"
-        for element in self.content:
-            output = output + str(element) + "\n"
-        output = output + ":END:\n"
-        return output
-
-class Node(OrgElement):
-    # Defines an OrgMode Node in a structure
-    # The ID is auto-generated using uuid.
-    # The level 0 is the document itself
-
+class Schedule(Plugin):
+    """Plugin for Schedule elements"""
     def __init__(self):
-        OrgElement.__init__(self)
-        self.content = []       
-        self.level = 0
-        self.heading = ""
-        self.priority = ""
-        self.tags = []
-        # TODO  Scheduling structure
+        Plugin.__init__(self)
+        self.regexp = re.compile("(?:\s*)(SCHEDULED|DEADLINE)(?::\s*)(<.*?>)(?:\s.*|$)")
+    def _treat(self,current,line):
+        scheduled = self.regexp.findall(line)
+        if scheduled:
+            current.append(self.Element(scheduled[0][0], scheduled[0][1]))
+        else:
+            self.treated = False
+        return current
+    class Element(OrgElement):
+        """Schedule is an element taking into account DEADLINE and SCHEDULED elements"""
+        DEADLINE = 1
+        SCHEDULED = 2
+        def __init__(self,type="",date=""):
+            OrgElement.__init__(self)
+            self.date = date
+            self.type = 0
+            if type == "DEADLINE":
+                self.type = self.DEADLINE
+            elif type == "SCHEDULED":
+                self.type = self.SCHEDULED
 
-    def __str__(self):
-        output = ""
+        def __str__(self):
+            """Outputs the Schedule element in text format (e.g SCHEDULED: <2010-10-10 10:10>)"""
+            if self.type == self.DEADLINE:
+                output = "DEADLINE:"
+            elif self.type == self.SCHEDULED:
+                output = "SCHEDULED:"
+            return output + " " + self.date + "\n"
 
-        if hasattr(self,"level"):
-            output = output + "*"*self.level
+class Drawer(Plugin):
+    """A Plugin for drawers"""
+    def __init__(self):
+        Plugin.__init__(self)
+        self.regexp = re.compile("^(?:\s*?)(?::)(\S.*?)(?::)\s*(.*?)$")
+    def _treat(self,current,line):
+        drawer = self.regexp.search(line)
+        if isinstance(current, Drawer.Element): # We are in a drawer
+            if drawer:
+                if drawer.group(1) == "END": # Ending drawer
+                    current = current.parent
+                elif drawer.group(2): # Adding a property
+                    current.append(self.Property(drawer.group(1),drawer.group(2)))
+            else: # Adding text in drawer
+                current.append(line.rstrip("\n"))
+        elif drawer: # Creating a drawer
+            current = current.append(Drawer.Element(drawer.group(1)))
+        else:
+            self.treated = False
+            return current
+        return current # It is a drawer, change the current also (even if not modified)
+    
+    class Element(OrgElement):
+        """A Drawer object, containing properties and text"""
+        def __init__(self,name=""):
+            OrgElement.__init__(self)
+            self.name = name
+        def __str__(self):
+            output = ":" + self.name + ":\n"
+            for element in self.content:
+                output = output + str(element) + "\n"
+            output = output + ":END:\n"
+            return output
+    class Property(OrgElement):
+        """A Property object, used in drawers."""
+        def __init__(self,name="",value=""):
+            OrgElement.__init__(self)
+            self.name = name
+            self.value = value
+        def __str__(self):
+            """Outputs the property in text format (e.g. :name: value)"""
+            return ":" + self.name + ": " + self.value
 
-        if self.parent is not None:
-            output = output + " "
-            if self.priority :
-                output = output + self.priority + " "
-            output = output + self.heading
+class Table(Plugin):
+    """A plugin for table managment"""
+    def __init__(self):
+        Plugin.__init__(self)
+        self.regexp = re.compile("^\s*\|")
+    def _treat(self,current,line):
+        table = self.regexp.match(line)
 
-            for tag in self.tags:
-                output= output + ":" + tag + ":"
+        if isinstance(current,self.Element):
+            if table:
+                current.append(line.rstrip("\n"))
+            else:
+                current = current.parent
+                self.treated = False
+        elif table:
+            current = current.append(self.Element())
+            current.append(line.rstrip("\n"))
+        else:
+            self.treated = False
+            return current
+        return current
 
-            output = output + "\n"
+    class Element(OrgElement):
+        """
+        A Table object
+        """
+        
+        def __init__(self):
+            OrgElement.__init__(self)
+        def __str__(self):
+            output = ""
+            for element in self.content:
+                output = output + str(element) + "\n"
+            return output
+
+class Node(Plugin):
+    def __init__(self):
+        Plugin.__init__(self)
+        self.regexp = re.compile("^(\*+)\s*(\[.*\])?\s*(.*)$")
+    def _treat(self,current,line):
+        heading = self.regexp.findall(line)
+        if heading: # We have a heading
+            if current.parent :
+                current.parent.append(current)
   
-        for element in self.content:
-            output = output + element.__str__()
+                  # Is that a new level ?
+            if (len(heading[0][0]) > current.level): # Yes
+                parent = current # Parent is now the current node
+            else:
+                parent = current.parent # If not, the parent of the current node is the parent
+                  # If we are going back one or more levels, walk through parents
+            while len(heading[0][0]) < current.level:
+                current = current.parent
+  
+                  # Creating a new node and assigning parameters
+            current = Node.Element() 
+            current.level = len(heading[0][0])
+            current.heading = re.sub(":([\w]+):","",heading[0][2]) # Remove tags
+            current.priority = heading[0][1]
+            current.parent = parent
+                  
+                  # Looking for tags
+            heading_without_links = re.sub(" \[(.+)\]","",heading[0][2])
+            current.tags = re.findall(":([\w]+):",heading_without_links)
+        else:
+            self.treated = False
+        return current
 
-        return output
+    class Element(OrgElement):
+        # Defines an OrgMode Node in a structure
+        # The ID is auto-generated using uuid.
+        # The level 0 is the document itself
+  
+        def __init__(self):
+            OrgElement.__init__(self)
+            self.content = []       
+            self.level = 0
+            self.heading = ""
+            self.priority = ""
+            self.tags = []
+          # TODO  Scheduling structure
+  
+        def __str__(self):
+            output = ""
+            
+            if hasattr(self,"level"):
+                output = output + "*"*self.level
+  
+            if self.parent is not None:
+                output = output + " "
+                if self.priority :
+                    output = output + self.priority + " "
+                output = output + self.heading
+  
+                for tag in self.tags:
+                    output= output + ":" + tag + ":"
+  
+                output = output + "\n"
+    
+            for element in self.content:
+                output = output + element.__str__()
+  
+            return output
 
-class DataStructure:
+class DataStructure(OrgElement):
     """
     Data structure containing all the nodes
     The root property contains a reference to the level 0 node
     """
-
     root = None
-
-    def append(self,node):
-        if node.parent is None: # Node has no parent (so it is the level 0 node)
-            self.root = node # So parent is the first added node
-        else:
-            node.parent.append(node)
-
+    def __init__(self):
+        OrgElement.__init__(self)
     def load_from_file(self,name):
-        current = Node()
-        parent = None
+        current = Node.Element()
+        current.parent = None
+        self.root = current
+ 
         file = open(name,'r')
 
-        re_drawer = re.compile("^(?:\s*?)(?::)(\S.*?)(?::)\s*(.*?)$")
-        re_heading = re.compile("^(\*+)\s*(\[.*\])?\s*(.*)$")
+        plugins = []
+        plugins.append(Table())
+        plugins.append(Drawer())
+        plugins.append(Node())
+        plugins.append(Schedule())
 
-        re_scheduled = re.compile("(?:\s*)(SCHEDULED|DEADLINE)(?::\s*)(<.*?>)(?:\s.*|$)")
-
-        current_drawer = None
         for line in file:
-            heading = re_heading.findall(line)
-
-            drawer = re_drawer.search(line)
-            scheduled = re_scheduled.findall(line)
-
-            if isinstance(current, Drawer):
-                if drawer:
-                    if drawer.group(1) == "END":
-                        current = current.parent
-                    elif drawer.group(2):
-                        current.append(Property(drawer.group(1),drawer.group(2)))
+            
+            for plugin in plugins:
+                current = plugin.treat(current,line)
+                if plugin.treated: # Plugin found something
+                    treated = True
+                    break;
                 else:
-                    current.append(line.rstrip("\n"))
-            elif drawer:
-                current = current.append(Drawer(drawer.group(1)))
-
-            elif heading: # We have a heading
-                self.append(current) # We append the current node as it is done
-
-                # Is that a new level ?
-                if (len(heading[0][0]) > current.level): # Yes
-                    parent = current # Parent is now the current node
-
-                # If we are going back one or more levels, walk through parents
-                while len(heading[0][0]) < current.level:
-                    current = current.parent
-
-                # Creating a new node and assigning parameters
-                current = Node() 
-                current.level = len(heading[0][0])
-                current.heading = re.sub(":([\w]+):","",heading[0][2]) # Remove tags
-                current.priority = heading[0][1]
-                current.parent = parent
-                
-                # Looking for tags
-                heading_without_links = re.sub(" \[(.+)\]","",heading[0][2])
-                current.tags = re.findall(":([\w]+):",heading_without_links)
-
-            elif scheduled:
-                current.append(Schedule(scheduled[0][0], scheduled[0][1]))
-            else: # Nothing special, just content
+                    treated = False
+            if not treated: # Nothing special, just content
                 if line is not None:
                     current.append(line)
 
         # Add the last node
+        # TODO: Using close functions of plugins
         if current.level>0:
-            self.append(current)
+            current.parent.append(current)
 
         file.close()
 
