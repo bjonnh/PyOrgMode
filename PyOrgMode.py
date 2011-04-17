a89f1f50e07586abb010ce5fef2443751d3d164d
Jonathan BISSON <bissonjonathan@gmail.com>
2011-04-17 22:24:23 +0200

    Version 0.03a (lot of changes see ChangeLog for details)

--- PyOrgMode.py
+++ PyOrgMode.py
@@ -31,22 +31,128 @@ import string
 import copy
 import time
 
+class OrgDate:
+    """Functions for date management"""
+
+    format = 0
+    TIMED = 1
+    DATED = 2
+    WEEKDAYED = 4
+    ACTIVE = 8
+    INACTIVE = 16
+
+    def __init__(self,value=None):
+        """
+        Initialisation of an OrgDate element.
+        """
+        if value != None:
+            self.set_value(value)
+
+    def set_value(self,value):
+        """
+        Setting the value of this element (automatic recognition of format)
+        """
+        # Checking whether it is an active date-time or not
+        if value[0]=="<":
+            self.format = self.format | self.ACTIVE
+            value = re.findall("(?:<)(.*)(?:>)",value)[0]
+        elif value[0]=="[":
+            self.format = self.format | self.INACTIVE
+            value = re.findall("(?:\[)(.*)(?:\])",value)[0]
+        # Checking if it is a date, a date+time or only a time
+        value_splitted = value.split()
+
+        timed = re.compile(".*?:.*?")
+        dated = re.compile(".*?-.*?-.*?")
+
+        if timed.findall(value):
+            self.format = self.format | self.TIMED
+        if dated.findall(value):
+            self.format = self.format | self.DATED
+
+        if len(value_splitted) == 3 :
+            # We have a three parts date so it's dated, timed and weekdayed
+            self.format = self.format | self.WEEKDAYED
+            self.value = time.strptime(value_splitted[0]+" "+value_splitted[2],"%Y-%m-%d %H:%M")
+        elif len(value_splitted) == 2 and (self.format & self.DATED) and not (self.format & self.TIMED):
+            # We have a two elements date that is dated and not timed. So we must have a dated weekdayed item
+            self.format = self.format | self.WEEKDAYED
+            self.value = time.strptime(value_splitted[0],"%Y-%m-%d")
+        elif self.format & self.TIMED:
+            # We have only a time
+            self.value = time.strptime(value,"%H:%M")
+        elif self.format & self.DATED:
+            self.value = time.strptime(value,"%Y-%m-%d")            
+
+    def get_value(self):
+        """
+        Get the timestamp as a text according to the format
+        """
+        if self.format & self.ACTIVE:
+            pre = "<"
+            post = ">"
+        elif self.format & self.INACTIVE:
+            pre = "["
+            post = "]"
+        else:
+            pre = ""
+            post = ""
+
+        if self.format & self.DATED:
+            # We have a dated event
+            dateformat = "%Y-%m-%d"
+            if self.format & self.WEEKDAYED:
+                # We have a weekday
+                dateformat = dateformat + " %a"
+            if self.format & self.TIMED:
+                # We have a time also
+                dateformat = dateformat + " %H:%M"
+
+            return pre+time.strftime(dateformat,self.value)+post
+
+        elif self.format & self.TIMED:
+            # We have a time only
+            timestr = time.strftime("%H:%M",self.value)
+            if timestr[0] == '0':
+                return timestr[1:]
+            return pre+timestr+post
+
 class OrgPlugin:
     """
     Generic class for all plugins
     """
     def __init__(self):
+        """ Generic initialization """
         self.treated = True
+        self.keepindent = True # By default, the plugin system stores the indentation before the treatment
+        self.keepindent_value = ""
+
     def treat(self,current,line):
+        """ This is a wrapper function for _treat. Asks the plugin if he can manage this kind of line. Returns True if it can """
         self.treated = True
-        return self._treat(current,line)
+        if self.keepindent :
+            self.keepindent_value = line[0:len(line)-len(line.lstrip(" \t"))] # Keep a trace of the indentation
+            return self._treat(current,line.lstrip(" \t"))
+        else:
+            return self._treat(current,line)
+
     def _treat(self,current,line):
+        """ This is the function used by the plugin for the management of the line. """
         self.treated = False
         return current
+
+    def _append(self,current,element):
+        """ Internal function that adds to current. """
+        if self.keepindent and hasattr(element,"set_indent"):
+            element.set_indent(self.keepindent_value)
+        return current.append(element)
+
     def close(self,current):
-        self.treated = True
+        """ A wrapper function for closing the module. """
+        self.treated = False
         return self._close(current)
     def _close(self,current):
+        """ This is the function used by the plugin to close everything that have been opened. """
         self.treated = False
         return current
 
@@ -58,6 +164,8 @@ class OrgElement:
         self.content=[]
         self.parent=None
         self.level=0
+        self.indent = ""
+
     def append(self,element):
         # TODO Check validity
         self.content.append(element)
@@ -67,15 +175,30 @@ class OrgElement:
             element.parent = self
         return element
 
-class Clock(OrgPlugin):
+    def set_indent(self,indent):
+        """ Transfer the indentation from plugin to element. """
+        self.indent = indent
+
+    def output(self):
+        """ Wrapper for the text output. """
+        return self.indent+self._output()
+    def _output(self):
+        """ This is the function really used by the plugin. """
+        return ""
+
+    def __str__(self):
+        """ Used to return a text when called. """
+        return self.output()
+
+class OrgClock(OrgPlugin):
     """Plugin for Clock elements"""
     def __init__(self):
         OrgPlugin.__init__(self)
-        self.regexp = re.compile("(?:\s*)CLOCK:(?:\s*)\[(.*)\]--\[(.*)\].*=>\s*(.*)")
+        self.regexp = re.compile("(?:\s*)CLOCK:(?:\s*)((?:<|\[).*(?:>||\]))--((?:<|\[).*(?:>||\])).*=>\s*(.*)")
     def _treat(self,current,line):
         clocked = self.regexp.findall(line)
         if clocked:
-            current.append(self.Element(clocked[0][0], clocked[0][1],clocked[0][2]))
+            self._append(current,self.Element(clocked[0][0], clocked[0][1], clocked[0][2]))
         else:
             self.treated = False
         return current
@@ -85,76 +208,90 @@ class Clock(OrgPlugin):
         TYPE = "CLOCK_ELEMENT"
         def __init__(self,start="",stop="",duration=""):
             OrgElement.__init__(self)
-            self.dateformat = "%Y-%m-%d %a %H:%M"
-            self.timeformat = "%H:%M"
-            self.start = self.convert_date(start)
-            self.stop = self.convert_date(stop)
-            self.duration = time.strptime(duration,self.timeformat)
-        def convert_date(self,date):
-            """Used to convert dates from a different TZ"""
-            return time.strptime(re.sub("\s(.*)\s"," ",date),"%Y-%m-%d %H:%M")
-        def format_duration(self,duration):
-            """Used to format durations identically to org-mode"""
-            timestr = time.strftime(self.timeformat,duration)
-            if timestr[0] == '0':
-                return timestr[1:]
-            return timestr
-        def __str__(self):
+            self.start = OrgDate(start)
+            self.stop = OrgDate(stop)
+            self.duration = OrgDate(duration)
+        def _output(self):
             """Outputs the Clock element in text format (e.g CLOCK: [2010-11-20 Sun 19:42]--[2010-11-20 Sun 20:14] =>  0:32)"""
-            return "CLOCK: [" + time.strftime(self.dateformat,self.start) + "]--["+ time.strftime(self.dateformat,self.stop) + "] =>  "+self.format_duration(self.duration)+"\n"
+            return "CLOCK: " + self.start.get_value() + "--"+ self.stop.get_value() + " =>  "+self.duration.get_value()+"\n"
 
-class Schedule(OrgPlugin):
+class OrgSchedule(OrgPlugin):
     """Plugin for Schedule elements"""
+    # TODO: Need to find a better way to do this
     def __init__(self):
         OrgPlugin.__init__(self)
-        self.regexp = re.compile("(?:\s*)(SCHEDULED|DEADLINE)(?::\s*)(<.*?>)(?:\s.*|$)")
+
+        self.regexp_scheduled = re.compile("SCHEDULED: ((<|\[).*?(>|\]))")
+        self.regexp_deadline = re.compile("DEADLINE: ((<|\[).*?(>|\]))")
+        self.regexp_closed = re.compile("CLOSED: ((<|\[).*?(>|\]))")
     def _treat(self,current,line):
-        scheduled = self.regexp.findall(line)
-        if scheduled:
-            current.append(self.Element(scheduled[0][0], scheduled[0][1]))
+        scheduled = self.regexp_scheduled.findall(line)
+        deadline = self.regexp_deadline.findall(line)
+        closed = self.regexp_closed.findall(line)
+  
+        if scheduled != []:
+            scheduled = scheduled[0][0]
+        if closed != []:
+            closed = closed[0][0]
+        if deadline != []:
+            deadline = deadline[0][0]
+
+        if scheduled or deadline or closed:
+            self._append(current,self.Element(scheduled, deadline,closed))
         else:
             self.treated = False
         return current
 
     class Element(OrgElement):
-        """Schedule is an element taking into account DEADLINE and SCHEDULED elements"""
+        """Schedule is an element taking into account DEADLINE, SCHEDULED and CLOSED parameters of elements"""
         DEADLINE = 1
         SCHEDULED = 2
+        CLOSED = 4
         TYPE = "SCHEDULE_ELEMENT"
-        def __init__(self,type="",date=""):
+        def __init__(self,scheduled=[],deadline=[],closed=[]):
             OrgElement.__init__(self)
-            self.date = date
             self.type = 0
-            if type == "DEADLINE":
-                self.type = self.DEADLINE
-            elif type == "SCHEDULED":
-                self.type = self.SCHEDULED
-
-        def __str__(self):
+  
+            if scheduled != []:
+                self.type = self.type | self.SCHEDULED
+                self.scheduled = OrgDate(scheduled)
+            if deadline != []:
+                self.type = self.type | self.DEADLINE
+                self.deadline = OrgDate(deadline)
+            if closed  != []:
+                self.type = self.type | self.CLOSED
+                self.closed = OrgDate(closed)
+  
+        def _output(self):
             """Outputs the Schedule element in text format (e.g SCHEDULED: <2010-10-10 10:10>)"""
-            if self.type == self.DEADLINE:
-                output = "DEADLINE:"
-            elif self.type == self.SCHEDULED:
-                output = "SCHEDULED:"
-            return output + " " + self.date + "\n"
+            output = ""
+            if self.type & self.SCHEDULED:
+                output = output + "SCHEDULED: "+self.scheduled.get_value()+" "
+            if self.type & self.DEADLINE:
+                output = output + "DEADLINE: "+self.deadline.get_value()+" "
+            if self.type & self.CLOSED:
+                output = output + "CLOSED: "+self.closed.get_value()+" "
+            if output != "":
+                output = output.rstrip() + "\n"
+            return output
 
-class Drawer(OrgPlugin):
+class OrgDrawer(OrgPlugin):
     """A Plugin for drawers"""
     def __init__(self):
         OrgPlugin.__init__(self)
         self.regexp = re.compile("^(?:\s*?)(?::)(\S.*?)(?::)\s*(.*?)$")
     def _treat(self,current,line):
         drawer = self.regexp.search(line)
-        if isinstance(current, Drawer.Element): # We are in a drawer
+        if isinstance(current, OrgDrawer.Element): # We are in a drawer
             if drawer:
                 if drawer.group(1) == "END": # Ending drawer
                     current = current.parent
                 elif drawer.group(2): # Adding a property
-                    current.append(self.Property(drawer.group(1),drawer.group(2)))
+                    self._append(current,self.Property(drawer.group(1),drawer.group(2)))
             else: # Adding text in drawer
-                current.append(line.rstrip("\n"))
+                self._append(current,line.rstrip("\n"))
         elif drawer: # Creating a drawer
-            current = current.append(Drawer.Element(drawer.group(1)))
+            current = self._append(current,OrgDrawer.Element(drawer.group(1)))
         else:
             self.treated = False
             return current
@@ -166,11 +303,11 @@ class Drawer(OrgPlugin):
         def __init__(self,name=""):
             OrgElement.__init__(self)
             self.name = name
-        def __str__(self):
+        def _output(self):
             output = ":" + self.name + ":\n"
             for element in self.content:
                 output = output + str(element) + "\n"
-            output = output + ":END:\n"
+            output = output + self.indent + ":END:\n"
             return output
     class Property(OrgElement):
         """A Property object, used in drawers."""
@@ -178,11 +315,11 @@ class Drawer(OrgPlugin):
             OrgElement.__init__(self)
             self.name = name
             self.value = value
-        def __str__(self):
+        def _output(self):
             """Outputs the property in text format (e.g. :name: value)"""
             return ":" + self.name + ": " + self.value
 
-class Table(OrgPlugin):
+class OrgTable(OrgPlugin):
     """A plugin for table managment"""
     def __init__(self):
         OrgPlugin.__init__(self)
@@ -206,7 +343,7 @@ class Table(OrgPlugin):
         TYPE = "TABLE_ELEMENT"
         def __init__(self):
             OrgElement.__init__(self)
-        def __str__(self):
+        def _output(self):
             output = ""
             for element in self.content:
                 output = output + "|"
@@ -215,10 +352,11 @@ class Table(OrgPlugin):
                 output = output + "\n"
             return output
 
-class Node(OrgPlugin):
+class OrgNode(OrgPlugin):
     def __init__(self):
         OrgPlugin.__init__(self)
         self.regexp = re.compile("^(\*+)\s*(\[.*\])?\s*(.*)$")
+        self.keepindent = False # If the line starts by an indent, it is not a node
     def _treat(self,current,line):
         heading = self.regexp.findall(line)
         if heading: # We have a heading
@@ -234,9 +372,9 @@ class Node(OrgPlugin):
                 # If we are going back one or more levels, walk through parents
                 while len(heading[0][0]) < current.level:
                     current = current.parent
-  
+                    parent = current.parent
             # Creating a new node and assigning parameters
-            current = Node.Element() 
+            current = OrgNode.Element() 
             current.level = len(heading[0][0])
             current.heading = re.sub(":([\w]+):","",heading[0][2]) # Remove tags
             current.priority = heading[0][1]
@@ -267,7 +405,7 @@ class Node(OrgPlugin):
             self.tags = []
           # TODO  Scheduling structure
   
-        def __str__(self):
+        def _output(self):
             output = ""
             
             if hasattr(self,"level"):
@@ -309,14 +447,11 @@ class Node(OrgPlugin):
 
             if hasattr(element,"content"):
                 for child in element.content:
-                    if hasattr(child,"heading"):
-                        print(child.heading)
-                    print("Level: "+str(level)+" Id: "+hex(id(child))+" Type: "+child.TYPE)
                     if hasattr(child,"parent"):
                         child.parent = element
                         self.reparent_cleanlevels(child,level+1)
 
-class DataStructure(OrgElement):
+class OrgDataStructure(OrgElement):
     """
     Data structure containing all the nodes
     The root property contains a reference to the level 0 node
@@ -326,17 +461,23 @@ class DataStructure(OrgElement):
     def __init__(self):
         OrgElement.__init__(self)
         self.plugins = []
-        self.load_plugins(Table(),Drawer(),Node(),Schedule(),Clock())
+        self.load_plugins(OrgTable(),OrgDrawer(),OrgNode(),OrgSchedule(),OrgClock())
         # Add a root element
         # The root node is a special node (no parent) used as a container for the file
-        self.root = Node.Element()
+        self.root = OrgNode.Element()
         self.root.parent = None
         self.level = 0
 
     def load_plugins(self,*arguments,**keywords):
+        """
+        Used to load plugins inside this DataStructure
+        """
         for plugin in arguments:
             self.plugins.append(plugin)
     def load_from_file(self,name):
+        """
+        Used to load an org-file inside this DataStructure
+        """
         current = self.root
         file = open(name,'r')
 
@@ -357,6 +498,9 @@ class DataStructure(OrgElement):
         file.close()
 
     def save_to_file(self,name,node=None):
+        """
+        Used to save an org-file corresponding to this DataStructure
+        """
         output = open(name,'w')
         if node == None:
             node = self.root
--- test_clock.py
+++ test_clock.py
@@ -7,16 +7,13 @@ import unittest
 class TestClockElement(unittest.TestCase):
     def test_duration_format(self):
         """Durations are formatted identically to org-mode"""
-        clock_elem = PyOrgMode.Clock.Element('2011-03-25 06:53',
-                                             '2011-03-25 09:12',
-                                             '2:19')
+
         for hour in '0', '1', '5', '10', '12', '13', '19', '23':
             for minute in '00', '01', '29', '40', '59':
                 orig_str = '%s:%s' % (hour, minute)
-                orig_tuple = time.strptime(orig_str,clock_elem.timeformat)
-                formatted_str = clock_elem.format_duration(orig_tuple)
+                orgdate_element = PyOrgMode.OrgDate(orig_str)
+                formatted_str = orgdate_element.get_value()
                 self.assertEqual(formatted_str, orig_str)
-
-
+  
 if __name__ == '__main__':
     unittest.main()
