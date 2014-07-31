0c23c74814bf90b1df4966c16a47b2eeb9b50808
Daniel Gonzalez (pegasus-mint) <gonvaled@gonvaled.com>
2014-07-31 17:15:43 +0200

    Preparing to push to pypi, and bumped version to 0.03b (tag needed for the github tarball)

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
-
-from setuptools import setup
+from distutils.core import setup
 
 setup(
-    name='PyOrgMode',
-    version='0.03a',
-    py_modules=['PyOrgMode'],
+    name = 'PyOrgMode',
+    packages = ['PyOrgMode'], # this must be the same as the name above
+    version = '0.03b',
+    description = 'Library to interact with org-mode from python',
+    author = 'Jonathan BISSON',
+    author_email = 'gonvaled@gonvaled.com', # I do not know the email address of the author
+    url = 'https://github.com/gonvaled/PyOrgMode', # use the URL to the github repo
+    download_url = 'https://github.com/gonvaled/PyOrgMode/tarball/0.03b', # I'll explain this in a second
+    keywords = ['org-mode'],
+    classifiers = [],
 )
