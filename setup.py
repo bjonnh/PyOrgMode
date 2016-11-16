23d89d428f0a30a24a06246f6e789485be8f86ea
jakirkham <jakirkham@gmail.com>
2016-11-16 11:02:39 -0500

    Fix the URLs to point to the main repo.

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     description = 'Library to interact with org-mode from python',
     author = 'Jonathan BISSON',
     author_email = 'gonvaled@gonvaled.com', # I do not know the email address of the author
-    url = 'https://github.com/gonvaled/PyOrgMode', # use the URL to the github repo
-    download_url = 'https://github.com/gonvaled/PyOrgMode/tarball/0.03b', # I'll explain this in a second
+    url = 'https://github.com/bjonnh/PyOrgMode', # use the URL to the github repo
+    download_url = 'https://github.com/bjonnh/PyOrgMode/tarball/0.03b', # I'll explain this in a second
     keywords = ['org-mode'],
     classifiers = [],
 )
